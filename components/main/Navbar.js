9a4b9fe986cedc61179dfe7e6fcfbecc5d130481
Daniel-Musunza <musunzafestus2019@gmail.com>
2024-01-17 10:04:12 +0300

    nav

--- components/main/Navbar.js
+++ components/main/Navbar.js
@@ -5,14 +5,13 @@ import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from "react-icons/ai";
 
 const Navbar = () => {
     const [nav, setNav] = useState(null)
-
     const handleNav=()=>{
         setNav(!nav)
     }
 
   return (
     <div className=" flex text-white w-full h-[80px] fixed top-0 z-50 md:justify-between items-center backdrop-blur-md p-11">
-        <h1 className=" font-light text-gray-400 text-2xl md:px-0 md:py-5 cursor-pointer px-0 titlep" onClick={handleNav} >PORTFOLIO</h1>
+        <h1 className=" font-light text-gray-400 text-2xl md:px-0 md:py-5 cursor-pointer px-0 titlep"><span className="md:hidden" style={{marginRight: '80px' }}></span>PORTFOLIO</h1>
         <ul className="hidden md:flex h-[50px] items-center w-[500px] justify-between border border-white bg-[#0300145e] rounded-full text-gray-200 font-extralight">
         <li><a href="#about" className="cursor-pointer ease-in duration-200 focus:bg-white focus:ring-1 hover:text-black hover:bg-white hover:rounded-full px-[20px] py-[15px]">
               About Me
@@ -41,7 +40,7 @@ const Navbar = () => {
 
         </div>
 
-        <div onClick={handleNav} className="md:hidden">
+        <div onClick={handleNav} className="md:hidden" style={{position: 'fixed', zIndex: '9999', marginRight: '500px'}}>
             {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25}  />  }
         </div>
 
@@ -59,11 +58,12 @@ const Navbar = () => {
             <li className="p-4 pl-8 text-xl border-b border-gray-600"><a href="#projects" className="cursor-pointer ease-in duration-200 hover:text-black hover:bg-white hover:rounded-full px-[20px] py-[15px]">
               Projects
             </a></li>
-          <li className="p-4 pl-8 text-xl border-b border-gray-600">
+            <li className="p-4 pl-8 text-xl"> GET IN TOUCH</li>
+          <li className="pl-20">
             <a href="https://api.whatsapp.com/send?phone=+254794711950" className="cursor-pointer ease-in duration-200  hover:text-black hover:bg-white hover:rounded-full px-[20px] py-[15px]" style={{marginRight: '30px', color: 'green'}}>
             <FaWhatsapp size={50}/>
             </a></li>
-            <li className="p-4 pl-8 text-xl border-b border-gray-600">
+            <li className="pl-20">
             <a href="mailto:musunzafestus2019@gmail.com" className="cursor-pointer ease-in duration-200  hover:text-black hover:bg-white hover:rounded-full px-[20px] py-[15px]">
             <AiOutlineMail size={50}/>
             </a></li>
